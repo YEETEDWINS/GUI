ef9d8eb6f3fa7645dcbbb02cfe9c87ab346bf03f
YEETEDWINS <164928968+YEETEDWINS@users.noreply.github.com>
2025-02-17 18:41:35 +0100

    Add files via upload

--- Rock Paper Scissors/main.py
+++ Rock Paper Scissors/main.py
@@ -1,5 +1,31 @@
 from tkinter import *
 import tkinter.font as fn
+import random
+
+scp = 0
+scc = 0
+
+def Game(player):
+  global scp, scc
+  choices = ["Rock", "Paper", "Scissors"]
+  cpu = random.choice(choices)
+  pselect.config(text="You Selected: "+player)
+  cselect.config(text="CPU Selected: "+cpu)
+
+  if cpu == player:
+    result.config(text="Tie...")
+  elif (player == "Rock" and cpu == "Scissors") or (player == "Paper" and cpu == "Rock") or (player == "Scissors" and cpu == "Paper"):
+    result.config(text="WINNNNNNN")
+    scp += 1
+  else:
+    result.config(text="What a loss")
+    scc += 1
+
+  percentage = round(scp/(scp+scc)*100, 2)
+  pscore.config(text="Your Score: "+str(scp))
+  cscore.config(text="CPU Score: "+str(scc))
+  ratio.config(text="Ratio: "+str(percentage))
+
 
 wn = Tk("RPS")
 wn.title("RPS")
@@ -24,13 +50,13 @@ mframe.pack()
 options = Label(mframe, text="Your Options:", font=baseF)
 options.grid(row=0, column=0, padx=10, pady=5)
 
-rock = Button(mframe, text="Rock", font=baseF)
+rock = Button(mframe, text="Rock", font=baseF, command=lambda:Game("Rock"))
 rock.grid(row=1, column=1, padx=10, pady=5)
 
-paper = Button(mframe, text="Paper", font=baseF)
+paper = Button(mframe, text="Paper", font=baseF, command=lambda:Game("Paper"))
 paper.grid(row=1, column=2, padx=10, pady=5)
 
-scissors = Button(mframe, text="Scissors", font=baseF)
+scissors = Button(mframe, text="Scissors", font=baseF, command=lambda:Game("Scissors"))
 scissors.grid(row=1, column=3, padx=10, pady=5)
 
 score = Label(mframe, text="Score:", font=baseF)
@@ -51,5 +77,9 @@ pscore.grid(row=0,column=1, padx=10)
 
 cscore = Label(bframe, text="CPU Score: ")
 cscore.grid(row=1,column=1, padx=10)
+
+# Ratio
+ratio = Label(wn, font=("", 20), text="Win Ratio: ")
+ratio.pack()
 
 wn.mainloop()
