fbf7b903c44fa6c372dfc3c8bba374893db24932
YEETEDWINS <164928968+YEETEDWINS@users.noreply.github.com>
2025-03-17 18:57:29 +0100

    Add files via upload

--- Address Book/main.py
+++ Address Book/main.py
@@ -1,7 +1,48 @@
 from tkinter import *
+from tkinter import messagebox
 
 wn = Tk("Address Book")
 wn.geometry("600x600")
+wn.title("Address Book")
+
+addressbook = {}
+
+def addUpdate():
+  personName = nameInput.get()
+  personMobile = mobileInput.get()
+  personAddress = addressInput.get()
+  personEmail = emailInput.get()                                                                                              
+  personDOF = dofInput.get()
+  if personName != "":
+    if personName not in addressbook:
+      storage.insert(0, personName)
+    addressbook[personName] = [personAddress, personMobile, personEmail, personDOF]
+    print(addressbook)
+  else:
+    messagebox.showwarning("Error", "No name has been prodivded")
+
+  clEntries()
+
+def clEntries():
+  nameInput.delete(0, END)
+  mobileInput.delete(0, END)
+  addressInput.delete(0, END)
+  emailInput.delete(0, END)
+  dofInput.delete(0, END)
+
+def editEntries():
+  clEntries()
+  item = storage.curselection()
+  if item:
+    selectedName = storage.get(item) 
+    nameInput.insert(0, selectedName)
+    details = addressbook[selectedName]
+    addressInput.insert(0, details[0])
+    mobileInput.insert(0, details[1])
+    emailInput.insert(0, details[2])
+    dofInput.insert(0, details[3])
+  else:
+    messagebox.showwarning("Error 101", "Select a name!!!")
 
 ## TOP FRAME
 tframe = Frame(wn)
@@ -27,7 +68,7 @@ leftframe.pack(side=LEFT, padx=20)
 storage = Listbox(leftframe, height=15, width=25)
 storage.pack()
 
-edit = Button(leftframe, text="Edit")
+edit = Button(leftframe, text="Edit", command=editEntries)
 edit.pack(side=LEFT, padx=30, ipadx=5)
 delete = Button(leftframe, text="Delete")
 delete.pack(side=LEFT)
@@ -64,7 +105,7 @@ dofInput.grid(row=5, column=2, padx=10, pady=10)
 hidden = Label(rightframe)
 hidden.grid(row=6, column=2, padx=10)
 
-addupd = Button(rightframe, text="Add / Update")
+addupd = Button(rightframe, text="Add / Update", command=addUpdate)
 addupd.grid(row=7, column=1, columnspan=2, ipadx=30, pady=0)
 
 wn.mainloop()
